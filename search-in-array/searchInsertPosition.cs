fa1c4c90d070114a632588db1e1075f4426a32d1
Goktug Erol <ge.dev@outlook.it>
2024-03-16 15:58:08 -0500

    Create searchInsertPosition.cs

---
+++ search-in-array/searchInsertPosition.cs
@@ -0,0 +1,62 @@
+/*
+*
+* Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+*
+*
+*
+*/
+
+// ===========================================================
+
+// Solution: 
+
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                // Target found, return the index
+                return mid;
+            } else if (nums[mid] < target) {
+                // Target is in the right half
+                left = mid + 1;
+            } else {
+                // Target is in the left half
+                right = mid - 1;
+            }
+        }
+
+        // If we reach here, target is not in the array
+        // Return the index where it should be inserted
+        return left;
+    }
+}
